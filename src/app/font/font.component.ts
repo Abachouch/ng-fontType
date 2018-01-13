e4bf1d03355e63ee13db6a27f56a4da5dd4db997
A.Youssouf <youssouf.abachouch@gmail.com>
2018-01-13 22:49:46 +0000

    fix prview type

--- src/app/font/font.component.ts
+++ src/app/font/font.component.ts
@@ -15,7 +15,12 @@ export class FontComponent implements OnInit {
   font: Font = new Font('untitled');
   instaledFonts = ['Roboto', 'open Sans', 'tahoma', 'verdana', 'shill'];
   weights = [{ v: '100', t: 'thin' }, { v: '400', t: 'normal' }, { v: '500', t: 'meduim' }, { v: '700', t: 'blod' }];
-  previewType: string;
+  previewType = 0;
+  previewText: string[] = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    `تسبب الثانية بـ ذات. عليها اعلان وكسبت جُل و, مئات فكان العالمية جُل مع. وتم عل هناك وعُرفت,
+    `,
+  ` 123 456 789 á	bé	cé	dé	é	ef	gé	há	ꟾ	ká	el	em	en	ó	pé	qv́	er	es	té	v́	ix`];
   previewStyle;
   constructor(
     private route: ActivatedRoute,
@@ -29,13 +34,12 @@ export class FontComponent implements OnInit {
       // construct object
       this.font.constructFromDatabase();
       console.log(title);
-    }else {
+    } else {
       this.font.fontFamily = 'tahoma';
       this.font.fontSize = 16;
       this.font.fontWeight = 400;
       this.font.lineHeight = 1;
       this.font.letterSpacing = 1;
-      this.previewType = 'latin';
     }
 
   }
