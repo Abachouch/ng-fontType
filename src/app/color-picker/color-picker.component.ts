7ef66ae460cc045c78c71f3fad5c702e513cadeb
A.Youssouf <youssouf.abachouch@gmail.com>
2018-01-15 23:59:35 +0000

    add color picker

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -3,14 +3,22 @@ import { ColorsService } from '../colors.service';
 
 @Component({
   selector: 'app-color-picker',
-  templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.scss']
+  templateUrl: './color-picker.component.html'
 })
 export class ColorPickerComponent implements OnInit {
   @Input() color;
   isOpen = false;
-  constructor(color: ColorsService) {
-    console.log(color.getHtmlColors());
+  colors: string[];
+
+  constructor(cs: ColorsService) {
+    console.log('cs.getallitem()');
+    console.log(cs.getAllHtml);
+
+    this.colors = cs.getAllHtml();
+    console.log('colors : ');
+    console.log(this.colors);
+
+
   }
 
   ngOnInit() {
