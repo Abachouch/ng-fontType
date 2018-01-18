71a75f2014dc2c16987cbda9780676828a643df0
A.Youssouf <youssouf.abachouch@gmail.com>
2018-01-18 23:40:00 +0000

    heroku fix

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-// server.js
-const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+"use strict";
+const express = require('express'),
+      app = express(),
+      morgan = require('morgan'),
+      path = require('path'),
+      portNumber = process.env.PORT || process.argv[2] || 8080;
+
+app.use(express.static(__dirname)); 
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+app.listen(portNumber, function () {
+  console.log("Listening on port " + portNumber);
+});
